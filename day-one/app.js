f2ca059e44550d735017dcb5781dd3a5a12f33e0
hursittarcan <hursittarcan@gmail.com>
2023-12-06 23:07:32 +0100

    day one - update

--- day-one/app.js
+++ day-one/app.js
@@ -1,11 +1,29 @@
 const fs = require('fs');
-
 const filePath = 'input.txt';
 
 fs.readFile(filePath, 'utf8', (err, data) => {
     const lines = data.split('\n');
 
+    //Parent for - loops over each line
     lines.forEach((line, index) => {
-        console.log(line);
+        let firstNumber, lastNumber;
+        let numericAmount = 0;
+
+        //Child 1 - loops over each char of selected line
+        for (let i = 0; i < line.length; i++) {
+            if (isFinite(line[i])) {
+                if (firstNumber === undefined) {
+                    firstNumber = line[i];
+                }
+
+                numericAmount++;
+
+                if (numericAmount > 2) {
+                    lastNumber = line[i];
+                }
+            }
+        }
+
+        console.log(`Line ${index + 1}: First: ${firstNumber}, Last: ${lastNumber}`);
     });
 });
