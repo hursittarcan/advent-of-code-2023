744cf764d85695db7eb4e91f86911985774346e9
hursittarcan <hursittarcan@gmail.com>
2023-12-06 23:29:05 +0100

    day one - code cleanup

--- day-one/app.js
+++ day-one/app.js
@@ -13,6 +13,11 @@ fs.readFile(filePath, 'utf8', (err, data) => {
                 if (firstNumber === undefined) {
                     firstNumber = line[i];
                 }
+
+                //First version of my solution checked whether the current line contains more than
+                //two numbers, since the challenge stated FIRST and LAST number, but turns out both can be the
+                //same, e.g: `eighttkbtzjz6nineeight` this only contains one numeric value -> 6, this is both the
+                //FIRST as the LAST value, soo this would become 66 in total for this specific line.
                 lastNumber = line[i];
             }
         }
@@ -22,7 +27,6 @@ fs.readFile(filePath, 'utf8', (err, data) => {
             totalCalibration += parseInt(totalLine);
         }
     });
-
     console.log(totalCalibration);
 });
 
