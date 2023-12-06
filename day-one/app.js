caeb6ca42835cda90b7aac493ea15723e2f0796f
hursittarcan <hursittarcan@gmail.com>
2023-12-06 23:30:39 +0100

    remove comment from js file + add explanatory readme file

--- day-one/app.js
+++ day-one/app.js
@@ -14,10 +14,6 @@ fs.readFile(filePath, 'utf8', (err, data) => {
                     firstNumber = line[i];
                 }
 
-                //First version of my solution checked whether the current line contains more than
-                //two numbers, since the challenge stated FIRST and LAST number, but turns out both can be the
-                //same, e.g: `eighttkbtzjz6nineeight` this only contains one numeric value -> 6, this is both the
-                //FIRST as the LAST value, soo this would become 66 in total for this specific line.
                 lastNumber = line[i];
             }
         }
---
+++ day-one/readme.md
@@ -0,0 +1,6 @@
+//First version of my solution checked whether the current line contains more than
+//two numbers, since the challenge stated FIRST and LAST number, but turns out both can be the
+//same, e.g: `eighttkbtzjz6nineeight` this only contains one numeric value -> 6, this is both the
+//FIRST as the LAST value, soo this would become 66 in total for this specific line.
+
+lastNumber = line[i];
