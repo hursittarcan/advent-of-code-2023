0180a06515243ddab665dd3625374869e8e1b51a
hursittarcan <hursittarcan@gmail.com>
2023-12-10 21:29:28 +0100

    part I ready for test

--- day-four/app.js
+++ day-four/app.js
@@ -3,6 +3,7 @@ const filePath = 'input.txt';
 
 fs.readFile(filePath, 'utf8', (err, data) => {
     const cards = data.split('\n');
+    let totalPoints = 0;
 
     for (let i = 0; i < cards.length; i++) {
         let game = (cards[i].split(':')[1]).trim();
@@ -17,15 +18,19 @@ fs.readFile(filePath, 'utf8', (err, data) => {
             if (!isNaN(parseInt(game.split(' | ')[1].split(' ')[i]))) {
                 let myNumber = game.split(' | ')[1].split(' ')[i];
                 if (winningNumbers.includes(parseInt(myNumber))) {
-                    console.log("INSIDE: " + myNumber);
                     totalWinningNumbersInGame++;
                 }
             }
         }
-        console.log("Total winning: " + totalWinningNumbersInGame);
-        console.log(game);
-        console.log(winningNumbers);
+
+        if (totalWinningNumbersInGame >= 1) {
+            let points = 1;
+            for (let point = 0; point < totalWinningNumbersInGame - 1; point ++) {
+                points *= 2;
+            }
+            totalPoints += points;
+        }
     }
 
-    //console.log(lines);
+    console.log(totalPoints);
 })
