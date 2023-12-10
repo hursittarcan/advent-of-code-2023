7acdc96f6088c8446a82fba2092ab51f9021cfa5
hursittarcan <hursittarcan@gmail.com>
2023-12-10 02:06:34 +0100

    app updated - part one passed

--- day-two/app.js
+++ day-two/app.js
@@ -4,32 +4,44 @@ const filePath = 'input.txt';
 fs.readFile(filePath, 'utf8', (err, data) => {
     const games = data.split('\n');
     const redCubes = 12, greenCubes = 13, blueCubes = 14;
+    let gameIdTotal = 0;
 
     for (let i = 0; i < games.length; i++) {
         let gameDetails = games[i];
         let gameId = gameDetails.split(':')[0].split(' ')[1];
 
         let gameSets = gameDetails.split(':')[1].split(';');
+        let failed = 0;
+
+        console.log("--- Game Set " + gameId + " ---");
         console.log(gameSets);
 
         for (let i = 0; i < gameSets.length; i++) {
-            // console.log(gameSets[i].split(',').length);
-            // console.log(gameSets[i].split(',')[0]);
-            // console.log(gameSets[i].split(',')[1]);
             let gameSetLength = gameSets[i].split(',').length;
+            let reds = 0, greens = 0, blues = 0;
+
             for (let j = 0; j < gameSetLength; j++) {
-                console.log(gameSets[i].split(',')[j]);
+                let setDetails= gameSets[i].split(',')[j].trim().split(' ');
+                if (setDetails[1] === "red") {
+                    reds += setDetails[0];
+                }
+                else if (setDetails[1] === "green") {
+                    greens += setDetails[0];
+                }
+                else if (setDetails[1] === "blue") {
+                    blues += setDetails[0];
+                }
             }
-            console.log("-----");
+
+            if (reds > redCubes || greens > greenCubes || blues > blueCubes) {
+                failed++;
+            }
         }
 
-        //console.log(gameDetails);
-        //console.log(gameId);
-
-
-        //Game 5: 3 green, 7 blue, 7 red; 6 green, 3 red, 4 blue; 7 blue, 4 red
-        //console.log(gameDetails)
-        console.log("--- SET ENDS HERE ---")
+        if (failed === 0) {
+            console.log("Game with ID " + gameId + " has passed.");
+            gameIdTotal += parseInt(gameId);
+        }
     }
-
+    console.log(gameIdTotal);
 })
