a300a0c3250cbc7c0a1fbeadbf0e69ee008fc3ea
hursittarcan <hursittarcan@gmail.com>
2023-12-10 12:07:33 +0100

    change input to test input

--- day-two/app.js
+++ day-two/app.js
@@ -5,6 +5,7 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     const games = data.split('\n');
     const redCubes = 12, greenCubes = 13, blueCubes = 14;
     let gameIdTotal = 0;
+    let gamePowerTotal = 0;
 
     for (let i = 0; i < games.length; i++) {
         let gameDetails = games[i];
@@ -16,42 +17,44 @@ fs.readFile(filePath, 'utf8', (err, data) => {
         console.log("--- Game Set " + gameId + " ---");
         console.log(gameSets);
 
+        let reds = 0, greens = 0, blues = 0;
+
         for (let i = 0; i < gameSets.length; i++) {
             let gameSetLength = gameSets[i].split(',').length;
-            let reds = 0, greens = 0, blues = 0;
 
             for (let j = 0; j < gameSetLength; j++) {
                 let setDetails= gameSets[i].split(',')[j].trim().split(' ');
                 if (setDetails[1] === "red") {
-                    if (setDetails[1] > reds) {
-                        reds = setDetails[1];
+                    if (setDetails[0] > reds) {
+                        console.log(setDetails[0])
+                        reds = setDetails[0];
                     }
                 }
                 else if (setDetails[1] === "green") {
-                    if (setDetails[1] > greens) {
-                        greens = setDetails[1];
+                    if (setDetails[0] > greens) {
+                        greens = setDetails[0];
                     }
                 }
                 else if (setDetails[1] === "blue") {
-                    if (setDetails[1] > blues) {
-                        blues = setDetails[1];
+                    if (setDetails[0] > blues) {
+                        blues = setDetails[0];
                     }
                 }
             }
-
-            console.log(reds);
-            console.log(greens);
-            console.log(blues);
-
-            if (reds > redCubes || greens > greenCubes || blues > blueCubes) {
-                failed++;
-            }
         }
 
-        if (failed === 0) {
-            console.log("Game with ID " + gameId + " has passed.");
-            gameIdTotal += parseInt(gameId);
-        }
+        console.log('---');
+        console.log(reds);
+        console.log(greens);
+        console.log(blues);
+        console.log('---');
+
+        gamePowerTotal += (reds * greens * blues);
+
+        // if (failed === 0) {
+        //     console.log("Game with ID " + gameId + " has passed.");
+        //     gameIdTotal += parseInt(gameId);
+        // }
     }
-    console.log(gameIdTotal);
+    console.log(gamePowerTotal);
 })
