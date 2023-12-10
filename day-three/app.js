5cc2627c8acbb0a375e0ae7e2b9b55bbb2491a33
hursittarcan <hursittarcan@gmail.com>
2023-12-10 20:39:25 +0100

    update - ex 3 - part I

--- day-three/app.js
+++ day-three/app.js
@@ -2,7 +2,16 @@ const fs = require('fs');
 const filePath = 'input.txt';
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-    const games = data.split('\n');
+    const engineSchematic = data.split('\n');
+    const symbols = ['*', '#', '+', '$'];
 
-    console.log(games);
+    for (let schematicDetails = 0; schematicDetails < engineSchematic.length; schematicDetails++) {
+        let currentSchematic = engineSchematic[schematicDetails];
+        //467..114..
+
+
+        console.log(engineSchematic[schematicDetails]);
+    }
+
+    console.log(engineSchematic);
 })
